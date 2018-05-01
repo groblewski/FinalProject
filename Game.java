9aa40dab382e78312f7e4ec57bca5f0fac3ca36b
musabeyli <34971131+musabeyli@users.noreply.github.com>
2018-05-01 12:51:18 -0500

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,5 +1,7 @@
 package application;
 
+import com.sun.javafx.tk.Toolkit.Task;
+
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
@@ -12,6 +14,11 @@ public class Game {
 	private HBox hb;
 	private VBox labels;
 	private VBox scores;
+	private Team team1;
+	private Team team2;
+	private  VBox lbls; 
+	private Label lbl1;
+	private Label lbl2;
 	public Game() {
 //		hb = new HBox();
 //		hb.setAlignment(Pos.CENTER);
@@ -26,11 +33,15 @@ public class Game {
 		hb.setAlignment(Pos.CENTER);
 		hb.setPadding(new Insets(10,10,10,10));
 		
-		labels = new VBox();
-		labels.setAlignment(Pos.TOP_CENTER);
-		labels.setSpacing(10);
-		labels.setPadding(new Insets(5, 0, 0, 0));
-		labels.getChildren().addAll(new Label("TBD"),new Label("TBD"));
+		
+		lbls = new VBox();
+	
+		lbls.setAlignment(Pos.TOP_CENTER);
+		lbls.setSpacing(10);
+		lbls.setPadding(new Insets(5, 0, 0, 0));
+		 lbl1 = new Label("TBD");
+		 lbl2 = new Label("TBD");
+		lbls.getChildren().addAll(lbl1,lbl2);
 		
 		VBox scores = new VBox();
 		TextField t1 = new TextField("Score");
@@ -40,15 +51,19 @@ public class Game {
 		scores.getChildren().addAll(t1, t2, new Button("Submit"));
 
 		
-		hb.getChildren().add(labels);
+		hb.getChildren().add(lbls);
 		hb.getChildren().add(scores);
 
 	}
 	
-	public Game(Team teamOne, Team teamTwo) {
+	public Game(Team teamOne, Team teamTwo, String game) {
+		this.team1 = teamOne;
+		this.team2 = teamTwo;
 		hb = new HBox();
 		hb.setAlignment(Pos.CENTER);
 		hb.setPadding(new Insets(10,10,10,10));
+		
+		
 		
 		labels = new VBox();
 		labels.setAlignment(Pos.TOP_CENTER);
@@ -61,17 +76,111 @@ public class Game {
 		t1.setPrefColumnCount(1);
 		TextField t2 = new TextField("Score");
 		t2.setPrefColumnCount(1);
-		scores.getChildren().addAll(t1, t2, new Button("Submit"));
+		Button newbie = new Button ("ERT");
+		scores.getChildren().addAll(t1, t2, newbie);
+
+		
+		
+		
+		newbie.setOnAction(e -> { //compares the scores
+			
+			Integer txt1 = Integer.valueOf(t1.getText());
+			Integer txt2 = Integer.valueOf(t2.getText());
+			
+			if (txt1 > txt2 ) {
+				//should give us the winner 
+				//put into the next game
+				
+				teamOne.setWinner(txt1);
+				Main main = new Main();
+				if (game == "firstRoundGameLeftOne") {
+					//main.quarterFinalGameLeftOne.team1 = teamOne;
+					//main.quarterFinalGameLeftOne.labels.getChildren().addAll(new Label(teamOne.getName()),new Label("TBD"));
+					//main.quarterFinalGameLeftOne.setLabels(teamOne, teamTwo);
+					
+					if (main.quarterFinalGameLeftOne.equals(null)) {
+						System.out.println("null");
+					}
+					else {
+						System.out.println(main.quarterFinalGameLeftOne.lbl1.getText());
+						main.quarterFinalGameLeftOne.lbl1.setText("face");
+						System.out.println(main.quarterFinalGameLeftOne.lbl1.getText());
+						//hb.getChildren().addAll(lbl1);
+						
+						
+					}
+					
+					//main.quarterFinalGameLeftOne.labels.set
+					if(main.firstRoundGameLeftTwo != null) {
+						
+						main.quarterFinalGameLeftOne.team2 = teamTwo;
+					}
+					
+					
+				}
+				if (game == "firstRoundGameLeftTwo") {
+					main.quarterFinalGameLeftOne.team2 = teamTwo;
+					if(main.firstRoundGameLeftOne != null) {
+						main.quarterFinalGameLeftOne.team1 = teamOne;
+						
+					}
+					
+				}
+				if (game == "firstRoundGameLeftThree") {
+					
+				}
+				if (game == "firstRoundGameLeftFour") {
+					
+				}
+				
+				if (game == "firstRoundGameRightOne") {
+					
+				}
+				
+				if (game == "firstRoundGameRightTwo") {
+					
+				}
+				if (game == "firstRoundGameRightThree") {
+					
+				}
+				if (game == "firstRoundGameRightFour") {
+					
+				}
+				
+				
+				
+				
+			}
+				
+				
+					
+			else {
+				System.out.println(txt2);
+			}
+		});
 
 		
 		hb.getChildren().add(labels);
 		hb.getChildren().add(scores);
 	}
 	
+	private void check () {
+		
+	}
+	
 	public void setLabels (Team teamOne, Team teamTwo) {
 		labels.getChildren().setAll(new Label(teamOne.getName()),new Label(teamTwo.getName()));
 		
 	}
+	
+	public void getTeamOneScore () {
+		
+	}
+	
+	public void getTeamTwoScore () {
+		
+	}
+	
 	
 	public HBox getBox() {
 		return hb;
