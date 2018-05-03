c1821ac5619ce5e28d8a4471d50d516a9c90ba62
Brennan <31396350+brenfife@users.noreply.github.com>
2018-05-03 12:19:53 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -64,7 +64,7 @@ public class Main extends Application {
 	
 	GridPane gPane = new GridPane();
 	
-	String looserR
+	//String looserR
 	String looserL;
 	
 	
@@ -91,10 +91,6 @@ public class Main extends Application {
 		teams.add(new Team("Paul", 6));
 		teams.add(new Team("David", 7));
 		teams.add(new Team("Lillian", 8));
-	
-		
-		
-		
 		
 		gPane.setAlignment(Pos.CENTER);
 		
@@ -106,51 +102,42 @@ public class Main extends Application {
 		}
 		
 		if (teams.size() == 1) {
-			;
 		}
 		
 		if (teams.size() > 1) {
 			championshipGame = new Game(null,null, "TBD");
-			if(teams.size() > 2) {
+			if (teams.size() > 2) {
 				rows.get(3).getChildren().addAll(championshipGame.getBox());
 				semiFinalGameLeft = new Game(null, null, "TBD");
 				semiFinalGameRight = new Game(null, null, "TBD");
-				if(teams.size() > 4) {
+				if (teams.size() > 4) {
 					rows.get(2).getChildren().addAll(semiFinalGameLeft.getBox());
 					rows.get(4).getChildren().addAll(semiFinalGameRight.getBox());
 					
-					
+
 					quarterFinalGameLeftOne = new Game(null, null, "TBD");
-					
-					
-					
-					
-					
 					quarterFinalGameLeftTwo = new Game(null, null, "TBD");
 					quarterFinalGameRightOne = new Game(null, null, "TBD");
 					quarterFinalGameRightTwo = new Game(null, null, "TBD");
-					if(teams.size() > 8) {
+					if (teams.size() > 8) {
 						
 					
 						
 						firstRoundGameLeftOne = new Game(teams.get(0), teams.get(15), "firstRoundGameLeftOne");
 						firstRoundGameLeftTwo = new Game(teams.get(2), teams.get(13), "firstRoundGameLeftTwo");
 						
-						
 						firstRoundGameLeftOne.newbie.setOnAction(new EventHandler<ActionEvent>() {
 							@Override
 							public void handle(ActionEvent event) {
-								
-								
 								int txt1 = Integer.parseInt(firstRoundGameLeftOne.t1.getText());
 								int  txt2 = Integer.parseInt(firstRoundGameLeftOne.t2.getText());
 
 								
-							if(txt1 > txt2 ) {
+							if (txt1 > txt2) {
 								//System.out.println(firstRoundGameLeftOne.lbl1.getText());
 								quarterFinalGameLeftOne.nl.setText(firstRoundGameLeftOne.nl.getText());
 							}
-							else if (txt1 < txt2 ) {
+							else if (txt1 < txt2) {
 								quarterFinalGameLeftOne.nl.setText(firstRoundGameLeftOne.nl2.getText());
 							}
 							else {
@@ -164,17 +151,16 @@ public class Main extends Application {
 						firstRoundGameLeftTwo.newbie.setOnAction(new EventHandler<ActionEvent>() {
 							@Override
 							public void handle(ActionEvent event) {
-								
-								
+
 								int txt1 = Integer.parseInt(firstRoundGameLeftTwo.t1.getText());
 								int  txt2 = Integer.parseInt(firstRoundGameLeftTwo.t2.getText());
 
 								
-							if(txt1 > txt2 ) {
+							if (txt1 > txt2) {
 								//System.out.println(firstRoundGameLeftOne.lbl1.getText());
 								quarterFinalGameLeftOne.nl2.setText(firstRoundGameLeftTwo.nl.getText());
 							}
-							else if (txt1 < txt2 ) {
+							else if (txt1 < txt2) {
 								quarterFinalGameLeftOne.nl2.setText(firstRoundGameLeftTwo.nl2.getText());
 							}
 							else {
@@ -183,9 +169,6 @@ public class Main extends Application {
 							
 							}
 						});
-						
-						
-						
 				
 						firstRoundGameLeftThree = new Game(teams.get(4), teams.get(11), "firstRoundGameLeftThree");
 						firstRoundGameLeftFour = new Game(teams.get(6), teams.get(9), "firstRoundGameLeftFour");
@@ -193,17 +176,15 @@ public class Main extends Application {
 						firstRoundGameLeftThree.newbie.setOnAction(new EventHandler<ActionEvent>() {
 							@Override
 							public void handle(ActionEvent event) {
-								
-								
 								int txt1 = Integer.parseInt(firstRoundGameLeftThree.t1.getText());
 								int  txt2 = Integer.parseInt(firstRoundGameLeftThree.t2.getText());
 
 								
-							if(txt1 > txt2 ) {
+							if (txt1 > txt2) {
 								//System.out.println(firstRoundGameLeftOne.lbl1.getText());
 								quarterFinalGameLeftTwo.nl.setText(firstRoundGameLeftThree.nl.getText());
 							}
-							else if (txt1 < txt2 ) {
+							else if (txt1 < txt2) {
 								quarterFinalGameLeftTwo.nl.setText(firstRoundGameLeftThree.nl2.getText());
 							}
 							else {
@@ -216,13 +197,11 @@ public class Main extends Application {
 						firstRoundGameLeftFour.newbie.setOnAction(new EventHandler<ActionEvent>() {
 							@Override
 							public void handle(ActionEvent event) {
-								
-								
 								int txt1 = Integer.parseInt(firstRoundGameLeftFour.t1.getText());
 								int  txt2 = Integer.parseInt(firstRoundGameLeftFour.t2.getText());
 
 								
-							if(txt1 > txt2 ) {
+							if (txt1 > txt2) {
 								//System.out.println(firstRoundGameLeftOne.lbl1.getText());
 								quarterFinalGameLeftTwo.nl2.setText(firstRoundGameLeftFour.nl.getText());
 							}
@@ -235,26 +214,21 @@ public class Main extends Application {
 							
 							}
 						});
-						
-						
-						
-						
+
 						firstRoundGameRightOne = new Game(teams.get(1), teams.get(14), "firstRoundGameRightOne");
 						
 						firstRoundGameRightOne.newbie.setOnAction(new EventHandler<ActionEvent>() {
 							@Override
 							public void handle(ActionEvent event) {
-								
-								
 								int txt1 = Integer.parseInt(firstRoundGameRightOne.t1.getText());
 								int  txt2 = Integer.parseInt(firstRoundGameRightOne.t2.getText());
 
 								
-							if(txt1 > txt2 ) {
+							if (txt1 > txt2) {
 								//System.out.println(firstRoundGameLeftOne.lbl1.getText());
 								quarterFinalGameRightOne.nl.setText(firstRoundGameRightOne.nl.getText());
 							}
-							else if (txt1 < txt2 ) {
+							else if (txt1 < txt2) {
 								quarterFinalGameRightOne.nl.setText(firstRoundGameRightOne.nl2.getText());
 							}
 							else {
@@ -263,26 +237,22 @@ public class Main extends Application {
 							
 							}
 						});
-						
-						
-						
+
 						
 						firstRoundGameRightTwo = new Game(teams.get(3), teams.get(12), "firstRoundGameRightTwo");
 						
 						firstRoundGameRightTwo.newbie.setOnAction(new EventHandler<ActionEvent>() {
 							@Override
 							public void handle(ActionEvent event) {
-								
-								
+
 								int txt1 = Integer.parseInt(firstRoundGameRightTwo.t1.getText());
 								int  txt2 = Integer.parseInt(firstRoundGameRightTwo.t2.getText());
 
-								
-							if(txt1 > txt2 ) {
+							if(txt1 > txt2) {
 								//System.out.println(firstRoundGameLeftOne.lbl1.getText());
 								quarterFinalGameRightOne.nl2.setText(firstRoundGameRightTwo.nl.getText());
 							}
-							else if (txt1 < txt2 ) {
+							else if (txt1 < txt2) {
 								quarterFinalGameRightOne.nl2.setText(firstRoundGameRightTwo.nl2.getText());
 							}
 							else {
@@ -292,26 +262,21 @@ public class Main extends Application {
 							}
 						});
 						
-						
-						
-						
 						firstRoundGameRightThree = new Game(teams.get(5), teams.get(10), "firstRoundGameRightThree");
 						
 						
 						firstRoundGameRightThree.newbie.setOnAction(new EventHandler<ActionEvent>() {
 							@Override
 							public void handle(ActionEvent event) {
-								
-								
 								int txt1 = Integer.parseInt(firstRoundGameRightThree.t1.getText());
 								int  txt2 = Integer.parseInt(firstRoundGameRightThree.t2.getText());
 
 								
-							if(txt1 > txt2 ) {
+							if (txt1 > txt2) {
 								//System.out.println(firstRoundGameLeftOne.lbl1.getText());
 								quarterFinalGameRightTwo.nl.setText(firstRoundGameRightThree.nl.getText());
 							}
-							else if (txt1 < txt2 ) {
+							else if (txt1 < txt2) {
 								quarterFinalGameRightTwo.nl.setText(firstRoundGameRightThree.nl2.getText());
 							}
 							else {
@@ -323,24 +288,20 @@ public class Main extends Application {
 						
 						
 						firstRoundGameRightFour = new Game(teams.get(7), teams.get(8), "firstRoundGameRightFour");
-						
-						
 						firstRoundGameRightFour.newbie.setOnAction(new EventHandler<ActionEvent>() {
 							@Override
 							public void handle(ActionEvent event) {
-								
-								
 								int txt1 = Integer.parseInt(firstRoundGameRightFour.t1.getText());
 								int  txt2 = Integer.parseInt(firstRoundGameRightFour.t2.getText());
 
 								
-							if(txt1 > txt2 ) {
+							if (txt1 > txt2) {
 								//System.out.println(firstRoundGameLeftOne.lbl1.getText());
 								quarterFinalGameRightTwo.nl2.setText(firstRoundGameRightFour.nl.getText());
 								
 								
 							}
-							else if (txt1 < txt2 ) {
+							else if (txt1 < txt2) {
 								quarterFinalGameRightTwo.nl2.setText(firstRoundGameRightFour.nl2.getText());
 							}
 							else {
@@ -351,27 +312,17 @@ public class Main extends Application {
 							}
 						});
 						
-						
-						
-						
 						quarterFinalGameLeftOne.newbie.setOnAction(new EventHandler<ActionEvent>() {
 							@Override
 							public void handle(ActionEvent event) {
-								//quarterFinalGameRightTwo = new Game(quarterFinalGameRightTwo.team1,quarterFinalGameRightTwo.team2, "quarterFinalGameRightTwo");
-								
-								//System.out.println(quarterFinalGameRightTwo == null);
-
 								int txt1 = Integer.parseInt(quarterFinalGameLeftOne.t1.getText());
 								int  txt2 = Integer.parseInt(quarterFinalGameLeftOne.t2.getText());
 
-
-								
-							if(txt1 > txt2 ) {
-								
+							if (txt1 > txt2) {
 								//semiFinalGameRight = new Game(teamOne,null, "TBD");
 								semiFinalGameLeft.nl.setText(quarterFinalGameLeftOne.nl.getText());
 							}
-							else if (txt1 < txt2 ) {
+							else if (txt1 < txt2) {
 								semiFinalGameLeft.nl.setText(quarterFinalGameLeftOne.nl2.getText());
 								
 							}
@@ -381,8 +332,6 @@ public class Main extends Application {
 							
 							}
 						});
-						
-						
 						
 						quarterFinalGameLeftTwo.newbie.setOnAction(new EventHandler<ActionEvent>() {
 							@Override
@@ -394,14 +343,12 @@ public class Main extends Application {
 								int txt1 = Integer.parseInt(quarterFinalGameLeftTwo.t1.getText());
 								int  txt2 = Integer.parseInt(quarterFinalGameLeftTwo.t2.getText());
 
-
-								
-							if(txt1 > txt2 ) {
+							if (txt1 > txt2) {
 								
 								//semiFinalGameRight = new Game(teamOne,null, "TBD");
 								semiFinalGameLeft.nl2.setText(quarterFinalGameLeftTwo.nl.getText());
 							}
-							else if (txt1 < txt2 ) {
+							else if (txt1 < txt2) {
 								semiFinalGameLeft.nl2.setText(quarterFinalGameLeftTwo.nl2.getText());
 								
 							}
@@ -411,11 +358,7 @@ public class Main extends Application {
 							
 							}
 						});
-						
-						
-					
-						
-						
+
 						quarterFinalGameRightTwo.newbie.setOnAction(new EventHandler<ActionEvent>() {
 							@Override
 							public void handle(ActionEvent event) {
@@ -426,14 +369,11 @@ public class Main extends Application {
 								int txt1 = Integer.parseInt(quarterFinalGameRightTwo.t1.getText());
 								int  txt2 = Integer.parseInt(quarterFinalGameRightTwo.t2.getText());
 
-
-								
-							if(txt1 > txt2 ) {
-								
+							if (txt1 > txt2) {
 								//semiFinalGameRight = new Game(teamOne,null, "TBD");
 								semiFinalGameRight.nl2.setText(quarterFinalGameRightTwo.nl.getText());
 							}
-							else if (txt1 < txt2 ) {
+							else if (txt1 < txt2) {
 								semiFinalGameRight.nl2.setText(quarterFinalGameRightTwo.nl2.getText());
 								
 							}
@@ -454,15 +394,13 @@ public class Main extends Application {
 
 								int txt1 = Integer.parseInt(quarterFinalGameRightOne.t1.getText());
 								int  txt2 = Integer.parseInt(quarterFinalGameRightOne.t2.getText());
-
-
 								
-							if(txt1 > txt2 ) {
+							if(txt1 > txt2) {
 								
 								//semiFinalGameRight = new Game(teamOne,null, "TBD");
 								semiFinalGameRight.nl.setText(quarterFinalGameRightOne.nl.getText());
 							}
-							else if (txt1 < txt2 ) {
+							else if (txt1 < txt2) {
 								semiFinalGameRight.nl.setText(quarterFinalGameRightOne.nl2.getText());
 								
 							}
@@ -484,16 +422,14 @@ public class Main extends Application {
 								int txt1 = Integer.parseInt(semiFinalGameLeft.t1.getText());
 								int  txt2 = Integer.parseInt(semiFinalGameLeft.t2.getText());
 
-
-								
-							if(txt1 > txt2 ) {
+							if (txt1 > txt2) {
 								
 								//semiFinalGameRight = new Game(teamOne,null, "TBD");
 								championshipGame.nl.setText(semiFinalGameLeft.nl.getText());
 								looserL = championshipGame.nl2.getText();
 								//Team looserLeft = 
 							}
-							else if (txt1 < txt2 ) {
+							else if (txt1 < txt2) {
 								championshipGame.nl.setText(semiFinalGameLeft.nl2.getText());
 								looserL = championshipGame.nl2.getText();
 								
@@ -517,16 +453,16 @@ public class Main extends Application {
 
 
 								
-							if(txt1 > txt2 ) {
+							if(txt1 > txt2) {
 								
 								//semiFinalGameRight = new Game(teamOne,null, "TBD");
 								championshipGame.nl2.setText(semiFinalGameRight.nl.getText());
-								looserR = semiFinalGameRight.t2.getText();
+								//looserR = semiFinalGameRight.t2.getText();
 								
 							}
-							else if (txt1 < txt2 ) {
+							else if (txt1 < txt2) {
 								championshipGame.nl2.setText(semiFinalGameRight.nl2.getText());
-								looserR = semiFinalGameRight.t1.getText();
+								//looserR = semiFinalGameRight.t1.getText();
 								
 							}
 							else {
@@ -535,7 +471,6 @@ public class Main extends Application {
 							
 							}
 						});
-						
 						
 						championshipGame.newbie.setOnAction(new EventHandler<ActionEvent>() {
 							@Override
@@ -548,32 +483,19 @@ public class Main extends Application {
 
 								int txt1 = Integer.parseInt(championshipGame.t1.getText());
 								int  txt2 = Integer.parseInt(championshipGame.t2.getText());//semiFinalGameRight
-
-
 								
-							if(txt1 > txt2 ) {
-								
+							if(txt1 > txt2) {	
 								//semiFinalGameRight = new Game(teamOne,null, "TBD");
-								//championshipGame.nl2.setText(semiFinalGameRight.nl.getText());
-								
-								String ans = "1st " + championshipGame.nl.getText();
-								
-								firsPlace = new Label(ans);
-								
+								//championshipGame.nl2.setText(semiFinalGameRight.nl.getText());								
+								String ans = "1st " + championshipGame.nl.getText();				
+								firsPlace = new Label(ans);		
 								String sec = "2nd" + championshipGame.nl2.getText();
-								Label secondPlace = new Label(sec);
-							
-								
+								Label secondPlace = new Label(sec);	
 								VBox rank = new VBox();
 								rank.getChildren().addAll(firsPlace,secondPlace);
 								rows.get(3).getChildren().setAll(rank);
-								
-								
-								
 							}
-							else if (txt1 < txt2 ) {
-								
-								
+							else if (txt1 < txt2) {
 								String ans = "1st " + championshipGame.nl2.getText();
 								
 								firsPlace = new Label(ans);
@@ -591,34 +513,19 @@ public class Main extends Application {
 							else {
 								//TIE
 							}
-							
-							
-							
-							
+
 							}
 						});
 					
-						
-						
-						
-						
 						rows.get(1).getChildren().addAll(quarterFinalGameLeftOne.getBox(),
-								quarterFinalGameLeftTwo.getBox());
-						
-						
-						
-						
+								quarterFinalGameLeftTwo.getBox());					
 						rows.get(5).getChildren().addAll(quarterFinalGameRightOne.getBox(),
-								quarterFinalGameRightTwo.getBox());
-						
-						
+								quarterFinalGameRightTwo.getBox());				
 						rows.get(0).getChildren().addAll(firstRoundGameLeftOne.getBox(), firstRoundGameLeftTwo.getBox(),
 								firstRoundGameLeftThree.getBox(),firstRoundGameLeftFour.getBox());
 						rows.get(7).getChildren().addAll(firstRoundGameRightOne.getBox(), firstRoundGameRightTwo.getBox(),
 								firstRoundGameRightThree.getBox(),firstRoundGameRightFour.getBox());
-						
-						
-						
+
 					} else {
 						quarterFinalGameLeftOne.setLabels(teams.get(0), teams.get(7));
 						quarterFinalGameRightOne.setLabels(teams.get(1), teams.get(6));
@@ -701,127 +608,59 @@ public class Main extends Application {
 		//quarterFinalGameLeftOne.lbl1.setText("H:");
 
 	}
-	
 
-	
 	public HBox makeTeamBoxLeft() {
-		
 		HBox hb = new HBox();
 		hb.setAlignment(Pos.CENTER);
 		hb.setPadding(new Insets(10,10,10,10));
-		
 		VBox labels = new VBox();
 		labels.setAlignment(Pos.TOP_CENTER);
 		labels.setSpacing(10);
 		labels.setPadding(new Insets(5, 0, 0, 0));
-		
-		
-		
-		
 		labels.getChildren().addAll(new Label("Team1"),new Label("Team2"));
-		
-	
-
-		
 		VBox scores = new VBox();
 		TextField t1 = new TextField("Score");
 		t1.setPrefColumnCount(1);
 		TextField t2 = new TextField("Score");
 		t2.setPrefColumnCount(1);
-		
-	
-		
 		scores.getChildren().addAll(t1, t2, new Button("Useless"));
-		
-		
-		
-		
 		hb.getChildren().add(labels);
 		hb.getChildren().add(scores);
-		
 		return hb;
 	}
 	
 public HBox makeTeamBoxRight() {
-		
 		HBox hb = new HBox();
 		hb.setAlignment(Pos.CENTER);
 		hb.setPadding(new Insets(10,10,10,10));
-		
 		VBox labels = new VBox();
 		labels.setAlignment(Pos.TOP_CENTER);
 		labels.setSpacing(10);
 		labels.setPadding(new Insets(5, 0, 0, 0));
 		labels.getChildren().addAll(new Label("Team1"),new Label("Team2"));
-		
 		VBox scores = new VBox();
 		TextField t1 = new TextField("Score");
 		t1.setPrefColumnCount(1);
 		TextField t2 = new TextField("Score");
 		t2.setPrefColumnCount(1);
 		scores.getChildren().addAll(t1, t2, new Button("Submit"));
-
 		hb.getChildren().add(scores);
 		hb.getChildren().add(labels);
-		
 		return hb;
 	}
 
 public VBox makeTeamBoxChampionship() {
-	
 	VBox vb = new VBox();
 	vb.setAlignment(Pos.CENTER);
 	vb.setPadding(new Insets(10,10,10,10));
-	
-
 	TextField t1 = new TextField("Score");
 	t1.setPrefColumnCount(1);
 	TextField t2 = new TextField("Score");
 	t2.setPrefColumnCount(1);
-
 	vb.getChildren().addAll(new Label("Team1"), t1, new Label("Team2"), t2, new Button("SAD"));
-	
 	return vb;
 }
 
-//public void helper () {
-//	
-//	Task<Integer> task = new Task<Integer>() {
-//        @Override
-//        protected Integer call() throws Exception {
-//            int i;
-//            for (i = 0; i < 1001; i++) {
-//                final int val = i;
-//                System.out.println(val);
-//                try {
-//                    Thread.sleep(1);
-//                } catch (InterruptedException ie) {
-//                }
-//
-//                // Update the GUI on the JavaFX Application Thread
-//                Platform.runLater(new Runnable() {
-//
-//                    @Override
-//                    public void run() {
-//                      quarterFinalGameLeftOne. 
-//                    }
-//                });
-//
-//            }
-//            return i;
-//        }
-//     };
-//  
-//
-//     Thread th = new Thread(task);
-//     th.setDaemon(true);
-//     th.start();
-//}
-		
-			
-	
-
-	
 	public static void main(String[] args) {
 		launch(args);
 	}
@@ -835,8 +674,5 @@ public VBox makeTeamBoxChampionship() {
             }
             return null;
         }
-         
     }
-	
-	
 }
