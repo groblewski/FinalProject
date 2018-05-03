03f6ec966b2fd747c8a051efb89ce27176e5a37c
JaredGlaub <jglaub@wisc.edu>
2018-05-03 18:36:16 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -585,6 +585,7 @@ public class Main extends Application {
 		
 		
 		Scene scene = new Scene(gPane, Color.BLACK);
+		scene.getStylesheets().addAll(this.getClass().getResource("application.css").toExternalForm());
 		primaryStage.setScene(scene);
 		primaryStage.show();
 		//quarterFinalGameLeftOne.lbl1.setText("H:");
