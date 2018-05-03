7d5dbcef20e1f1e438c7a12cb7e4f331537ff5b1
Brennan <31396350+brenfife@users.noreply.github.com>
2018-05-03 14:00:09 -0500

    Update Team.java

--- Team.java
+++ Team.java
@@ -14,25 +14,17 @@
 package application;
 
 public class Team {
-	
-	private int seed;
 	private String name;
-	private int previousScore;
 	private int scoreTeam;
 	
-	
 	public Team() {
-		this.seed = 0;
 		this.name = null;
-		this.previousScore = 0;
 	}
 	
-	public Team(String name, int seed){
-		this.seed = seed;
+	public Team(String name){
 		this.name = name;
 		this.previousScore = 0;
 	}
-	
 	
 	public void setWinner(int score) {
 		this.scoreTeam = score;
@@ -42,15 +34,6 @@ public class Team {
 		return scoreTeam;
 	}
 	
-	
-	public int getSeed() {
-		return this.seed;
-	}
-	
-	public void setSeed(int seed) {
-		this.seed = seed;
-	}
-	
 	public String getName() {
 		return this.name;
 	}
@@ -58,13 +41,4 @@ public class Team {
 	public void setName(String name) {
 		this.name = name;
 	}
-	
-	public int getPreviousScore() {
-		return this.previousScore;
-	}
-	
-	public void setPreviousScore(int previousScore) {
-		this.previousScore = previousScore;
-	}
-
 }
