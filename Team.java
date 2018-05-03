b6676091e907fc464b062e588085c69e26713d0d
groblewski <37050435+groblewski@users.noreply.github.com>
2018-05-03 18:07:40 -0500

    Update Team.java

--- Team.java
+++ Team.java
@@ -14,51 +14,57 @@
 package application;
 
 public class Team {
+	
+	private int seed;
 	private String name;
+	private int previousScore;
 	private int scoreTeam;
 	
-	/**
-	 * No argument constructor
-	 */
+	
 	public Team() {
+		this.seed = 0;
 		this.name = null;
+		this.previousScore = 0;
 	}
 	
-	/**
-	 * constructs Team object with a given name
-	 * @param name name of team
-	 */
-	public Team(String name){
+	public Team(String name, int seed){
+		this.seed = seed;
 		this.name = name;
+		this.previousScore = 0;
 	}
 	
-	/*
-	 * Sets winner and associates score
-	 * @param score score of winner
-	 */
+	
 	public void setWinner(int score) {
 		this.scoreTeam = score;
 	}
 	
-	/*
-	 * gets winner
-	 */
 	public int getWinner() {
 		return scoreTeam;
 	}
 	
-	/*
-	 * Gets name
-	 */
+	
+	public int getSeed() {
+		return this.seed;
+	}
+	
+	public void setSeed(int seed) {
+		this.seed = seed;
+	}
+	
 	public String getName() {
 		return this.name;
 	}
 	
-	/*
-	 * Sets name
-	 * @param name name to be set 
-	 */
 	public void setName(String name) {
 		this.name = name;
 	}
+	
+	public int getPreviousScore() {
+		return this.previousScore;
+	}
+	
+	public void setPreviousScore(int previousScore) {
+		this.previousScore = previousScore;
+	}
+
 }
