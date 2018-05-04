305d0fc9cba5ff9f5d69fe97eebf8160cad118ea
Bguth3 <32819071+Bguth3@users.noreply.github.com>
2018-05-03 19:52:55 -0500

    Update Game.java

--- Game.java
+++ Game.java
@@ -27,22 +27,26 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
+/**
+* Creates an instance of a game between two Teams. Adds GUI elements to the 
+* screen. These elements can be edited 
+* 
+* Bugs: No known bugs
+*
+* @author Emma Groblewski
+* @author Brian Guth
+*/
 public class Game {
 	public HBox hb;
 	public VBox labels;
 	public VBox scores;
 	public Team team1;
 	public Team team2;
-	//private  VBox lbls; 
 	Label nl;
 	Label nl2;
-	//public Label lbl1;
-	//public Label lbl2;
-	
 	public Button newbie;
 	public int txt1;
 	public int txt2;
-	
 	public TextField t1;
 	public TextField t2;
 	private int loserScore;
@@ -61,61 +65,63 @@ public class Game {
 		labels.setSpacing(10);
 		labels.setPadding(new Insets(5, 0, 0, 0));
 		
-	if (team1 == null || team2 == null) {
-		nl = new Label("TBD");
-		nl2 = new Label("TBD");
-		labels.getChildren().addAll(nl,nl2);
-		hb.getChildren().add(labels);
+		if (team1 == null || team2 == null) {
+			nl = new Label("TBD");
+			nl2 = new Label("TBD");
+			labels.getChildren().addAll(nl,nl2);
+			hb.getChildren().add(labels);
 		
-	}
+		}
 	
-	else {
-		nl = new Label(teamOne.getName());
-		System.out.println(nl + " is nl");
-		nl2 = new Label(teamTwo.getName());
-		labels.getChildren().addAll(nl,new Label(teamTwo.getName()));
-		hb.getChildren().add(labels);
-
-	}
+		else {
+			nl = new Label(teamOne.getName());
+			System.out.println(nl + " is nl");
+			nl2 = new Label(teamTwo.getName());
+			labels.getChildren().addAll(nl,new Label(teamTwo.getName()));
+			hb.getChildren().add(labels);
+		}
 	
-	scores = new VBox();
-	scores.setAlignment(Pos.CENTER_RIGHT);
-	 t1 = new TextField("Score");
-	t1.setPrefColumnCount(4);
-	 t2 = new TextField("Score");
-	t2.setPrefColumnCount(4);
-	 		 
-	newbie = new Button ("Submit");
-	scores.getChildren().addAll(t1, t2, newbie);
-	
-	hb.getChildren().add(scores);
-	
+		scores = new VBox();
+		scores.setAlignment(Pos.CENTER_RIGHT);
+		t1 = new TextField("Score");
+		t1.setPrefColumnCount(4);
+		t2 = new TextField("Score");
+		t2.setPrefColumnCount(4);
+	 	newbie = new Button ("Submit");
+		scores.getChildren().addAll(t1, t2, newbie);
+		hb.getChildren().add(scores);
 	}
 
-	public void getTeamOneScore () {
-		
-	}
-	
-	public void getTeamTwoScore () {
-		
-	}
-	
+	/**
+	* Returns the HBox
+	* @return hb
+	*/
 	public HBox getBox() {
 		return hb;
 	}
-
+	/**
+	* Returns the losers Score
+	* @return loserScore
+	*/
 	public int getLoserScore() {
 		return loserScore;
 	}
-
+	/**
+	* Sets the losers Score
+	*/
 	public void setLoserScore(int loserScore) {
 		this.loserScore = loserScore;
 	}
-
+	/**
+	* Returns the losing team
+	* @return loserTeam
+	*/
 	public String getLoserTeam() {
 		return loserTeam;
 	}
-
+	/**
+	* Sets the losing team
+	*/
 	public void setLoserTeam(String team12) {
 		this.loserTeam = team12;
 	}
